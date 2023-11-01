8bfce757ccf45f14481e24d63b4b4f2579083b51
David Deko <davedeksy@yahoo.com>
2023-11-01 16:46:54 -0500

    Update 101

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World! David 12345";
-    }
+    // @RequestMapping("/")
+    // String home() {
+    //     return "Hello World!";
+    // }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
